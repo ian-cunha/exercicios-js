f4a83270934056301c83375dcc67fe48ad1359c6
Ian <69704125+ian-cunha@users.noreply.github.com>
2022-04-08 05:02:35 -0300

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,10 @@
+// Boas práticas
+// Usar let não var
+
+let num1 = 4;
+let num2 = 4;
+
+console.log('Soma: 2 + 4 = ' + (num1 + num2));
+console.log('Subtração: 2 - 4 = ' + (num1 - num2));
+console.log('Divisão: 2 / 4 = ' + (num1 / num2));
+console.log('Multiplicação: 2 * 4 = ' + (num1 * num2));
