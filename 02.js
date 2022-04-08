565286e7826fa2b34884d4a68fcf2133f01463b7
Ian <69704125+ian-cunha@users.noreply.github.com>
2022-04-08 05:45:13 -0300

    Update 02.js

--- 02.js
+++ 02.js
@@ -2,7 +2,9 @@
 
 let valorJantar = 80;
 let taxaGarcom = 10;
-let total = valorJantar + ((taxaGarcom / 100) * valorJantar);
+let total = 0;
+
+total = valorJantar + ((taxaGarcom / 100) * valorJantar);
 
 console.log(`Valor do jantar: R$${valorJantar},00`);
 console.log(`Valor do garçom: R$${(taxaGarcom / 100) * valorJantar},00`);
